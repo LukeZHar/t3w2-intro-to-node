bba760f9e464ff848973c96638fa7db075442539
LukeZHar <15649@coderacademy.edu.au>
2024-10-29 12:31:55 +1000

    feat:try catch added to dice roller

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-const inquirer = require('inquirer').default;
+// const inquirer = require('inquirer').default;
+const promptSync = require('prompt-sync')();
+
 
 console.log("Welcome to the dice rolling game!")
 
@@ -10,6 +12,9 @@ console.log("Welcome to the dice rolling game!")
  * @returns {number} The result of the dice roll
  */
 function rollDice(diceSide=6) {
+    if(diceSide <= 0) {
+        throw new Error("Please enter a number greater than 0");
+    }
     let diceResults = 0;
     diceResults = Math.ceil(Math.random() * diceSide);
     return diceResults;
@@ -17,13 +22,28 @@ function rollDice(diceSide=6) {
 
 // console.log("The dice roll is: " + rollDice());
 
-const prompt = inquirer.createPromptModule();
-prompt([
-    {
-        type: "number",
-        name: "dice_side",
-        message: "How many sided dice do you want to roll?",
+const diceSide = promptSync("How many sided dice do you want to roll? ");
+
+try {
+    const parsedDiceSide = parseInt(diceSide);
+    if (isNaN(parsedDiceSide)) {
+        throw new Error("Please enter a number");
     }
-]).then((answer) => {
-    console.log(rollDice(answer.dice_side));
-});
+    const diceResults = rollDice(parsedDiceSide);
+    console.log(`You rolled a ${diceResults}.`);
+} catch (error) {
+    console.error("You got an error!");
+    console.log(error);
+}
+
+
+// const prompt = inquirer.createPromptModule();
+// prompt([
+//     {
+//         type: "number",
+//         name: "dice_side",
+//         message: "How many sided dice do you want to roll?",
+//     }
+// ]).then((answer) => {
+//     console.log(rollDice(answer.dice_side));
+// });
